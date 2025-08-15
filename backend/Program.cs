cc2ed5f75af484bd6c87e2223d03dac27dfbfbaa
mariacedersten <s2401554@edu.bc.fi>
2025-08-15 13:34:08 +0300

    Add reading Hello message from file

--- backend/Program.cs
+++ backend/Program.cs
@@ -1,6 +1,25 @@
+using Microsoft.JSInterop.Infrastructure;
+
 var builder = WebApplication.CreateBuilder(args);
 var app = builder.Build();
 
-app.MapGet("/", () => "Hello World!");
+app.MapGet("/", () => "Hello World!"); // Using "anonomous functions"
+app.MapGet("hellous/", HelloMehthod);
 
 app.Run();
+
+string HelloMehthod()
+{
+    var helloFolder = new DirectoryInfo(Directory.GetCurrentDirectory());
+    var helloPath = Path.Combine(helloFolder.FullName, "hello.txt");
+
+
+    // print to console absolute path (FullName)
+    Console.WriteLine($"Reading hello from: {helloPath}");
+    // in spe
+
+    var message = File.ReadAllText(helloPath);
+    return "Read from FILE:\n\n" + message;
+
+
+}
